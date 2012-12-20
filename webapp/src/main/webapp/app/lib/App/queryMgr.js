228785cb1c4c40cdc8ec7ee3dea90d46cc380464
Cesar Martinez Izquierdo <cesar.izq@gmail.com>
2012-12-20 17:23:36 +0100

    Disable the BlackList execution rule for this cube

--- webapp/src/main/webapp/app/lib/App/queryMgr.js
+++ webapp/src/main/webapp/app/lib/App/queryMgr.js
@@ -531,7 +531,7 @@ App.queryMgr = function(options) {
     return {
         init: function() {
         	addExecutionRule(new App.LimitQuerySizeRule());
-        	addExecutionRule(new App.BlackListQueries());
+        	//addExecutionRule(new App.BlackListQueries());
             createNewQuery();
         },
         events: _events,
